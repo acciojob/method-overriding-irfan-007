751401e156b87c8ac8702923bf66a5520f2d54bf
irfan <85334710+irfan-007@users.noreply.github.com>
2024-03-13 19:21:21 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
-public class Main {
-  
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+        B obj1=new B();
+        System.out.println(obj1.meth());       
+        B obj=new B();
+        System.out.println(obj.meth());  
+        
+	}
+}
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A {
+    String meth(){
+        return "Method is overridden in Extended class B";
+    }
 }
